72f8aba21d5f9e80939b86c85b50d3b51a11d462
Evgeniy Opryshko <eopryshko@ronasit.com>
2021-09-29 23:34:29 +0300

    Refactored and sped up pygame rendering.
    Added quit event handling to make window
    not freeze on MacOS.

--- bruhai/test.py
+++ bruhai/test.py
@@ -1,12 +1,12 @@
 import random
 from enum import IntEnum
 from time import sleep
-from typing import Type, Optional
+from typing import Callable
 
 import gym
 from gym3 import ToGymEnv
 
-from bruhai.rendering import Renderer
+from bruhai.rendering import Renderer, RendererScreenSettings
 
 
 class Actions(IntEnum):
@@ -32,36 +32,52 @@ MOVE_ACTIONS = (
     Actions.UP_LEFT,
 )
 
+SLEEP_TIME_BETWEEN_GAMES = 0.1
+
+RendererFactory = Callable[[RendererScreenSettings], Renderer]
+
 
 class Test:
-    def __init__(self, renderer_class: Type[Renderer] = None, render_n_frame: int = 1):
-        self.env: ToGymEnv = gym.make("procgen:procgen-heist-v0")
+    def __init__(self, renderer_factory: RendererFactory = None, render_n_frame: int = 1):
+        self.env: ToGymEnv = gym.make(
+            "procgen:procgen-heist-v0",
+            use_backgrounds=False,
+            use_monochrome_assets=True,
+            restrict_themes=True,
+        )
         assert render_n_frame > 0
-        self.renderer: Optional[Renderer] = renderer_class(64, 64, 8) if renderer_class else None
+        if renderer_factory:
+            self.renderer = renderer_factory(RendererScreenSettings(64, 64, 8))
         self.render_n_frame = render_n_frame
         self.episode_num = 0
 
     def run(self) -> None:
         for episode in range(10):
-            self.next_episode()
+            should_continue = self.next_episode()
+            if not should_continue:
+                break
         self.env.close()
 
-    def next_episode(self) -> None:
+    def next_episode(self) -> bool:
         self.episode_num += 1
         print(f"Episode #{self.episode_num}")
         obs = self.env.reset()
         step_num = 0
         if self.renderer:
             self.renderer.render(obs)
-        sleep(1)
-        reward = 0
+        total_reward = 0
         while True:
+            step_num += 1
             rand_action = random.choice(MOVE_ACTIONS)
             nobs, reward, done, _ = self.env.step(rand_action)
+            total_reward += reward
             if self.renderer and step_num % self.render_n_frame == 0:
-                self.renderer.render(nobs)
+                should_continue = self.renderer.render(nobs)
+                if not should_continue:
+                    print(f"Quit on `{step_num}` steps with reward `{total_reward}`.")
+                    return False
             if done:
                 break
-            step_num += 1
-        print(f"Finished in `{step_num}` steps with reward `{reward}`.")
-        sleep(1)
+        print(f"Finished in `{step_num}` steps with reward `{total_reward}`.")
+        sleep(SLEEP_TIME_BETWEEN_GAMES)
+        return True
--- scripts/test.py
+++ scripts/test.py
@@ -3,6 +3,6 @@ from bruhai.test import Test
 
 if __name__ == "__main__":
     Test(
-        renderer_class=PyGameRenderer,
-        render_n_frame=16,
+        renderer_factory=PyGameRenderer,
+        render_n_frame=2,
     ).run()
